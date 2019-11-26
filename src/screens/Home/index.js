336a7830c35647864cc09a2e16844a70dfc2a402
Nilton Lopes <niltonrck@gmail.com>
2019-11-26 02:35:26 -0300

    WIP: added logic to animate search input

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 import Input from "../../components/Forms/Input";
 
 export default function Home() {
+  const [isSearch, setIsSearch] = useState(false);
+
+  function makeSearch(term) {
+    setIsSearch(true);
+  }
+
   return (
     <div className="home-container">
-      <div className="search-container">
-        <Input placeholder="Pesquisar" icon="search" />
+      <div className={`search-container ${isSearch ? "active" : ""}`}>
+        <Input
+          placeholder="Pesquisar"
+          icon="search"
+          onClick={value => makeSearch(value)}
+        />
       </div>
+      <div className="body-container">body</div>
     </div>
   );
 }
