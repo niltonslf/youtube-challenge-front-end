3de62ed6b11a09025b0e42ba4033cc986dbd6fb1
Nilton Lopes <niltonrck@gmail.com>
2019-12-01 23:03:00 -0300

    REFACTOR: remove unused detail component

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 import Home from "./screens/Home"
-import Detail from "./screens/Detail"
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Home} />
-        {/* <Route path="/video/:id" component={Detail} /> */}
       </Switch>
     </BrowserRouter>
   )
